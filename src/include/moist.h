8a33e045625e7b7c87b3755803c725be001d8f04
AGeissler <achim.geissler@intergga.ch>
2016-03-08 17:41:37 +0100

    Work in progress, extend moisture calculation by additional model from Otto PhD Thesis.
    Code save.

---
+++ src/include/moist.h
@@ -0,0 +1,13 @@
+      PARAMETER (LEGACY=1)
+      PARAMETER (OTTO=2)
+
+C Moisture model common for file version and active
+C model storage
+      common/moist85/iMoistModel(MCOMP),iMoistVer(MCOMP)
+
+      integer iMoistModel  !  Moisture model used:
+                           !  LEGACY = 1
+                           !  OTTO = 2
+
+      integer iMoistVer    !  File version
+
